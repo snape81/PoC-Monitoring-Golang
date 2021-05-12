aa7b4c2b8efab0fca34bd078e78fac7271e15281
francescobianca <francescobianca96@gmail.com>
2021-05-12 18:28:41 +0200

    fix

--- Monitoring/Monitoring.go
+++ Monitoring/Monitoring.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Printf("Hello Monitoring")
+	// Read date unix system
+	date, err := exec.Command("date").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The date is %s\n", date)
+
+	// Read mem unix system
+	mem, err := exec.Command("free", "-m").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Memory usage:\n")
+	fmt.Printf("%s\n", mem)
+
+	// Read disk unix system
+	disk, err := exec.Command("df", "-h").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Disk usage:\n")
+	fmt.Printf("%s\n", disk)
 }
