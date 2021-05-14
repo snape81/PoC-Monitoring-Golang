b4f23cf47cdb23118d5abc51672610dcf7b61392
francescobianca <francescobianca96@gmail.com>
2021-05-14 12:25:03 +0200

    add logging

--- Logging/Logging.go
+++ Logging/Logging.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("Hello Logging")
+	fmt.Println("Logging Command!")
+
+	args := []string{"journalctl", "-n 200", "--no-pager"}
+	output, err := RunCMD("sudo", args, true)
+
+	if err != nil {
+		fmt.Println("Error:", output)
+	} else {
+		fmt.Println("Result:", output)
+	}
+
+}
+
+// RunCMD is a simple wrapper around terminal commands
+func RunCMD(path string, args []string, debug bool) (out string, err error) {
+
+	cmd := exec.Command(path, args...)
+
+	var b []byte
+	b, err = cmd.CombinedOutput()
+	out = string(b)
+
+	if debug {
+		fmt.Println(strings.Join(cmd.Args[:], " "))
+
+		if err != nil {
+			fmt.Println("RunCMD ERROR")
+			fmt.Println(out)
+		}
+	}
+
+	return
 }
